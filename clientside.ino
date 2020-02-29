72a741bfe1fa170427a4d0c3b1248473b2c8585f
MimesisProtezy <61520152+MimesisProtezy@users.noreply.github.com>
2020-02-29 15:51:18 +0100

    Add files via upload
    Code dedicated to operating a band which is responsible for readouts of EMG signals and passing them via wifi to another ESP32.

---
+++ clientside.ino
@@ -0,0 +1,94 @@
+
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-client-server-wi-fi/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "ESP32-Access-Point";
+const char* password = "put-your-pswd-here";
+
+//Your IP address or domain name with URL path
+const char* serverNameemg1 = "http://192.168.4.1/emg1";
+const char* serverNameemg2 = "http://192.168.4.1/emg2";
+const char* serverNameemg3 = "http://192.168.4.1/emg3";
+
+#include <Wire.h>
+
+
+String EMG1;
+String EMG2;
+String EMG3;
+
+unsigned long previousMillis = 0;
+const long interval = 5; 
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  
+  if(currentMillis - previousMillis >= interval) {
+     // Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED ){ 
+      EMG1 = httpGETRequest(serverNameemg1);
+      EMG2 = httpGETRequest(serverNameemg2);
+      EMG3 = httpGETRequest(serverNameemg3);
+      Serial.println("emg1: " + EMG1 + " emg2:" + EMG2 + " emg3:" + EMG3);
+  
+      
+      // save the last HTTP GET Request
+      previousMillis = currentMillis;
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+  }
+}
+
+String httpGETRequest(const char* serverName) {
+  HTTPClient http;
+    
+  // Your IP address with path or Domain name with URL path 
+  http.begin(serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "--"; 
+  
+  if (httpResponseCode>0) {
+    //Serial.print("HTTP Response code: ");
+    //Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  // Free resources
+  http.end();
+
+  return payload;
+}
